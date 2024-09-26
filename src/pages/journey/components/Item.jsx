58e1454cd12468f1bb2ec13bca328982937358c9
Oscar Ndodo <oscarndodo12@gmail.com>
2024-09-26 14:54:16 +0200

    problema entre class para className resolvido.

--- src/pages/journey/components/Item.jsx
+++ src/pages/journey/components/Item.jsx
@@ -22,7 +22,7 @@ function Item({ image, name, progress }) {
                     {name}
                 </h6>
 
-                <p class="text-slate-500 text-sm line-clamp-1 my-1 w-full overflow-clip">
+                <p className="text-slate-500 text-sm line-clamp-1 my-1 w-full overflow-clip">
                     <span className="bg-white rounded-3xl text-xs py-0.5 px-3 text-black/80">Software</span>
                     <span className="bg-white rounded-3xl text-xs py-0.5 mx-1 px-3 text-black/80">Software</span>
                 </p>
@@ -30,15 +30,15 @@ function Item({ image, name, progress }) {
 
                 {progress >= 0 && (
                     <div className="w-full flex mt-1">
-                        <span id="ProgressLabel" class="sr-only">Loading</span>
+                        <span id="ProgressLabel" className="sr-only">Loading</span>
 
                         <span
                             role="progressbar"
                             aria-labelledby="ProgressLabel"
                             className="block w-full rounded-full bg-white/90"
                         >
-                            <span class="flex items-center justify-center w-full h-3 rounded-full bg-gradient-to-r from-purple-600 to-indigo-700 text-center text-xs" style={{ width: progress + "%" }}>
-                                <span class="font-bold text-white text-[10px]  line-clamp-1"> {progress}%</span>
+                            <span className="flex items-center justify-center w-full h-3 rounded-full bg-gradient-to-r from-purple-600 to-indigo-700 text-center text-xs" style={{ width: progress + "%" }}>
+                                <span className="font-bold text-white text-[10px]  line-clamp-1"> {progress}%</span>
                             </span>
                         </span>
                     </div>
